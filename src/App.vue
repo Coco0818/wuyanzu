78723c3d52dabfec8c9c0c8a19e4e7d7eacc9ff1
Misstear <756489192@qq.com>
2020-11-14 14:19:34 +0800

    feat:test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,55 @@
 <template>
-  <div></div>
+  <div class="serach-container">
+    <div class="search-header">
+      <div class="searchTab">
+        <a href="javascript:;">职位(500+)</a>
+        <a href="javascript:;">公司(500+)</a>
+      </div>
+      <div class="search-keyword">
+        <el-input
+          placeholder="搜索职位、公司或地点"
+          class="search-input"
+        ></el-input>
+        <el-button class="search-button">搜索</el-button>
+        <!-- <input type="text" placeholder="搜索职位、公司或地点" /> -->
+      </div>
+      <div class="search-title">
+        <span class="search-title-tip">相关搜索：</span>
+        <ul class="search-title-ul">
+          <li class="search-title-content">java后端</li>
+          <li class="search-title-content">java web</li>
+          <li class="search-title-content">java实习</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {}
+export default {};
 </script>
 <style scoped>
+.search-header {
+  height: 180px;
+  background-color: #f2f5f4;
+}
+
+.search-keyword {
+  display: flex;
+}
+.search-button {
+  background-color: #00b38a;
+  color: #fff;
+}
+.search-title-tip {
+  display: inline-block;
+  font-size: 14px;
+  margin-right: 7px;
+  margin-top: 10px;
+  height: 22px;
+  color: #777;
+  line-height: 22px;
+}
+search-title-content {
+  font-size: 14px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import element from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(element);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   router,
-  store
-}).$mount('#app')
+  store,
+}).$mount("#app");
