f57f3ee785a741e523bf431e8a911443ef65a30c
Coco0818 <nikeyouxiang@qq.com>
2020-11-14 13:22:29 +0800

    fix:'修改了样式重置'

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 组件
+import Header from '@/components/Header'
+
 Vue.config.productionTip = false
+
+
+// 全局的公共组件
+Vue.component('Header', Header)
+
 
 new Vue({
   render: h => h(App),
