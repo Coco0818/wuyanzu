830ec67330b5b9de2d88dfcaa616b79d9ca519df
HSg666 <1303574731@qq.com>
2020-11-17 17:55:51 +0800

    feat: '合并'

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,10 @@
 </template>
 <script>
 import Home from '@/pages/Home'
-
 export default {
   name: 'App',
   components: {
-<<<<<<< HEAD
-    // Login,
-    // Register,
-    Detail
-=======
     Home,
->>>>>>> 5d525d0bddc0567d7da7070f25025d028f6219d9
   },
 }
 </script>
