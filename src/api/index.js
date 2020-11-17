878da5a4986309b1dc95ef430d6a926a0caaf97c
Coco0818 <nikeyouxiang@qq.com>
2020-11-17 09:26:39 +0800

    feat: '拆出热门区内容‘

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,6 @@ export const reqCompany = () => ajax.get('/positions')
 
 // 三级分类
 export const reqCateGory = () => ajax.get('/categorys')
+
+//  互联网热门公司
+export const reqInterCompany = () => ajax.get('/companys')
