b7e91d880ab31c525a0bb52ea3c239d5e1b1921f
Coco0818 <nikeyouxiang@qq.com>
2020-11-17 18:41:51 +0800

    feat:合并

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,7 @@ import Home from '@/pages/Home'
 export default {
   name: 'App',
   components: {
-<<<<<<< HEAD
-    // Login,
-    // Register,
-    Detail
-=======
     Home,
->>>>>>> 5d525d0bddc0567d7da7070f25025d028f6219d9
   },
 }
 </script>
