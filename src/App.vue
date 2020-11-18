48879b7b7485164c3bee8eb3dff411b415c8d00f
Coco0818 <nikeyouxiang@qq.com>
2020-11-18 20:10:30 +0800

    fix:小修

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div>
-    <!-- <Home /> -->
     <router-view />
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import Home from "@/pages/Home";
-
-=======
-import Home from '@/pages/Home'
->>>>>>> 7f1792cac23343585df0be97a78b25249c207a74
 export default {
-  name: "App",
-  components: {
-    Home,
-  },
-};
+  name: 'App',
+}
 </script>
 <style scoped>
 </style>
